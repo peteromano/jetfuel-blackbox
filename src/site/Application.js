d3e3922bf067fb76fe15e9e48201b577d8347f93
Pete Romano <pete.romano@yahoo.com>
2012-09-07 17:50:57 -0400

    Using new espresso vendor lib version 0.6 and refactoring boilerplate

---
+++ src/site/Application.js
@@ -0,0 +1,43 @@
+espresso.Application('Application', function($) {
+    'use strict';
+
+    return {
+
+        initialize: function(config, context) {
+
+        },
+
+        ready: function() {
+
+        }
+
+    };
+
+}, {
+
+    Services: {
+        /*basePath: 'site.services',
+        attributes: '@withDependencies',
+        autoload: ['Router'],
+        registry: {
+            Router:         '{basePath}.Router {attributes}'
+        }*/
+    },
+
+    Configuration: {
+
+        Default: {
+
+            services: {
+
+                /*Router: {
+
+                }*/
+
+            }
+
+        }
+
+    }
+
+});
